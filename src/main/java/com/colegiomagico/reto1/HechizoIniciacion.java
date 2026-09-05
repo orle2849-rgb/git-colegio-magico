131a1ad564d79c289cbce084ee6318f8304ee366
orle2849-rgb <orle2849@gmail.com>
2026-09-05 15:54:08 +0000

    feat: reto 1 solucionado

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Esteban - estudiante: Esteban Suarez"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
